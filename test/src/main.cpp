651f1b41976838711d92279dfe5f51f00e01a880
namelessvoid <pommesdiefritte@gmx.de>
2014-04-11 21:30:28 +0200

    Implemented class Line and tests.

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,12 +1,15 @@
 #include <QtTest>
 
 #include "algorithmtest.h"
+#include "linetest.h"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
     AlgorithmTest algorithmTest;
+    LineTest lineTest;
 
-    return QTest::qExec(&algorithmTest, argc, argv);
+    return QTest::qExec(&algorithmTest, argc, argv) |
+            QTest::qExec(&lineTest, argc, argv);
 }
 
