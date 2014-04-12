0518d23dc4005b650e96dd87f9abfcf341dbcec8
namelessvoid <pommesdiefritte@gmx.de>
2014-04-12 15:25:56 +0200

    Added EDL internal methods.

--- algorithms/include/edl/edl.h
+++ algorithms/include/edl/edl.h
@@ -3,6 +3,11 @@
 
 #include "algorithm.h"
 
+#include "line.h"
+
+#include <opencv2/core/core.hpp>
+#include <vector>
+
 class EDL : public Algorithm
 {
 public:
@@ -10,6 +15,13 @@ public:
     ~EDL();
 
     void calculate();
+
+private:
+    void calcGradAndAngle(cv::InputArray gradientX, cv::InputArray gradientY, cv::OutputArray gradientMagnitude, cv::OutputArray gradientAngle);
+
+    void calculateAnchors(cv::InputArray gradientMagnitude, cv::OutputArray anchorPoints, int threshold);
+
+    void routeAnchors(cv::InputArray gradientMagnitude, cv::InputArray gradientAngle, cv::InputArray anchorPoints, std::vector<Line*> result);
 };
 
 #endif // EDL_H
--- algorithms/src/edl/edl.cpp
+++ algorithms/src/edl/edl.cpp
@@ -1,5 +1,7 @@
 #include "edl/edl.h"
 
+#include <opencv2/imgproc/imgproc.hpp>
+
 EDL::EDL()
 {
 
@@ -11,6 +13,24 @@ EDL::~EDL()
 }
 
 void EDL::calculate()
+{
+    // call calcgradAndAngle
+    // call calculateAnchors
+    // call routeAnchors
+    // Save result
+}
+
+void calcGradAndAngle(cv::InputArray gradientX, cv::InputArray gradientY, cv::OutputArray gradientMagnitude, cv::OutputArray gradientAngle)
+{
+
+}
+
+void calculateAnchors(cv::InputArray gradientMagnitude, cv::OutputArray anchorPoints, int threshold)
+{
+
+}
+
+void routeAnchors(cv::InputArray gradientMagnitude, cv::InputArray gradientAngle, cv::InputArray anchorPoints, std::vector<Line*> result)
 {
 
 }
