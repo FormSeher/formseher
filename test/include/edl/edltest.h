d85731e9697b88c4f9f99b86a89c7b9720ebb5fe
namelessvoid <pommesdiefritte@gmx.de>
2014-04-13 00:13:43 +0200

    Created EDLTest::routeAnchorsTest().

--- test/include/edl/edltest.h
+++ test/include/edl/edltest.h
@@ -6,6 +6,8 @@
 
 #include <opencv2/core/core.hpp>
 
+#include <iostream>
+
 // brute force everything public (don't do this at home kids!)
 #undef private
 #undef protected
@@ -46,6 +48,38 @@ private slots:
 
         point.x = 1;
         QVERIFY(VERTICAL == edl->getDirection(point, angles));
+    }
+
+    void routeAnchorsTest()
+    {
+        cv::Mat magnitudes(5, 5, CV_32F);
+
+        cv::Mat_<double> _magnitudes(magnitudes);
+        _magnitudes(1, 1) = 15.0;
+        _magnitudes(1, 2) = 14.0;
+        _magnitudes(1, 3) = 14.3;
+
+        cv::Mat angles(5, 5, CV_32F);
+        cv::Mat_<double> _angles(angles);
+        _angles(1, 1) = 0.5;
+        _angles(1, 2) = 0.5;
+        _angles(1, 3) = 0.5;
+
+        std::vector<cv::Point*> anchorPoints;
+        anchorPoints.push_back(new cv::Point(1, 1));
+        anchorPoints.push_back(new cv::Point(1, 3));
+
+        std::vector<Line*> result;
+
+        edl->routeAnchors(magnitudes, angles, anchorPoints, result);
+
+        for(auto anchorPoint : anchorPoints)
+            delete anchorPoint;
+    }
+
+    void walkFromAnchorTest()
+    {
+
     }
 
     void cleanupTestCase()
