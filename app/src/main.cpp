6b32eaa6ab7cfb6634d023c22403c3bb9f806d85
namelessvoid <pommesdiefritte@gmx.de>
2014-04-13 20:19:08 +0200

    Replaced app/main() with simple approach.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,11 +1,54 @@
+//#include <mainwindow.h>
+//#include <QApplication>
+
+//int main(int argc, char *argv[])
+//{
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+
+//    return a.exec();
+//}
+
 #include <mainwindow.h>
+
 #include <QApplication>
+#include <iostream>
+#include <vector>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include "edl/edl.h"
+#include "line.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    if(argc < 2)
+    {
+        std::cout << "Call with path to an image file!" << std::endl;
+        return 0;
+    }
 
-    return a.exec();
+    cv::Mat input = cv::imread(argv[1]);
+    if(!input.data)
+    {
+        std::cerr << "Invalid input file. Must be an image!" << std::endl;
+        std::cerr << "Could not load image '" << argv[1] << "'!" << std::endl;
+        return -1;
+    }
+
+    EDL edl;
+    edl.setInput(argv[1]);
+    edl.calculate();
+
+    std::vector<Line*>* result = edl.getResult();
+    std::cout << "lines found: " << result->size() << std::endl;
+
+    for(auto line : *result)
+        delete line;
+    delete result;
+
+    cv::waitKey(0);
+    return 0;
 }
