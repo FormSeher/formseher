f4d17ac9508f941691fb82af90124f3860ecd5b4
namelessvoid <pommesdiefritte@gmx.de>
2014-04-19 19:32:40 +0200

    Added anwies's MainWindow.

--- app/include/mainwindow.h
+++ app/include/mainwindow.h
@@ -15,6 +15,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_openpicture1_clicked();
+
+    void on_openpicture2_clicked();
+
+    void on_save1_clicked();
+
+    void on_save2_clicked();
+
+    void on_lineEdit1_returnPressed();
+
+    void on_lineEdit2_returnPressed();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
