fc2538092541f8b6f1ada3558518e401c850205e
namelessvoid <pommesdiefritte@gmx.de>
2014-05-11 16:07:07 +0200

    ObjectGraph now stores nodes and edges.

--- algorithms/include/objectdetection/olga/objectgraph.h
+++ algorithms/include/objectdetection/olga/objectgraph.h
@@ -1,7 +1,11 @@
 #ifndef OBJECTGRAPH_H
 #define OBJECTGRAPH_H
 
+#include <vector>
 #include <opencv2/core/core.hpp>
+
+#include "objectdetection/olga/objectgraphedge.h"
+#include "objectdetection/olga/objectgraphnode.h"
 
 class ObjectGraph
 {
@@ -15,6 +19,10 @@ public:
      * 0.0 (0%) if they are disjunct or 1.0 (100%) if they are equal.
      */
     float compareTo(const ObjectGraph& objectGraph) const;
+
+private:
+    std::vector<ObjectGraphNode> nodes;
+    std::vector<ObjectGraphEdge> edges;
 };
 
 #endif // OBJECTGRAPH_H
