b2b47446051df9f6b5e6343cff24bcb88ae55c2e
namelessvoid <pommesdiefritte@gmx.de>
2014-05-18 20:42:11 +0200

    Overwrote implementation app/main.cpp with OLGA stuff.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,29 +1,50 @@
 #include <gui/mainwindow.h>
 #include <QApplication>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 #include "gui/algorithmconfigdialog.h"
 #include "gui/edl/edlconfigdialog.h"
 #include "gui/hough/houghconfigdialog.h"
 
+#include "objectdetection/olga/objectgraph.h"
+#include "gui/objectdetection/olga/objectgraphvisualizer.h"
+
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    formseher::ObjectGraph objectGraph;
+    formseher::ObjectGraphNode* node1 = objectGraph.insertNode(10, 100);
+    formseher::ObjectGraphNode* node2 = objectGraph.insertNode(300, 50);
+    formseher::ObjectGraphNode* node3 = objectGraph.insertNode(30, 150);
+    formseher::ObjectGraphNode* node4 = objectGraph.insertNode(50, 100);
+    objectGraph.insertEdge(node1, node2);
+    objectGraph.insertEdge(node2, node3);
+    objectGraph.insertEdge(node1, node3);
+    objectGraph.insertEdge(node4, node3);
 
-    formseher::EDLConfigDialog edlDialog1;
-    formseher::EDLConfigDialog edlDialog2;
+    cv::Mat graphMatrix;
+    formseher::ObjectGraphVisualizer::drawGraphOnMatrix(&objectGraph, graphMatrix);
+    cv::imshow("object graph", graphMatrix);
+    cv::waitKey(0);
 
-    formseher::HoughConfigDialog houghDialog1;
-    formseher::HoughConfigDialog houghDialog2;
+//    QApplication a(argc, argv);
 
-    formseher::MainWindow w;
+//    formseher::EDLConfigDialog edlDialog1;
+//    formseher::EDLConfigDialog edlDialog2;
+
+//    formseher::HoughConfigDialog houghDialog1;
+//    formseher::HoughConfigDialog houghDialog2;
+
+//    formseher::MainWindow w;
 
 
-    w.registerAlgorithmConfigDialog(1, "EDL", static_cast<formseher::AlgorithmConfigDialog*>(&edlDialog1));
-    w.registerAlgorithmConfigDialog(2, "EDL", static_cast<formseher::AlgorithmConfigDialog*>(&edlDialog2));
-    w.registerAlgorithmConfigDialog(1, "Hough", static_cast<formseher::AlgorithmConfigDialog*>(&houghDialog1));
-    w.registerAlgorithmConfigDialog(2, "Hough", static_cast<formseher::AlgorithmConfigDialog*>(&houghDialog2));
+//    w.registerAlgorithmConfigDialog(1, "EDL", static_cast<formseher::AlgorithmConfigDialog*>(&edlDialog1));
+//    w.registerAlgorithmConfigDialog(2, "EDL", static_cast<formseher::AlgorithmConfigDialog*>(&edlDialog2));
+//    w.registerAlgorithmConfigDialog(1, "Hough", static_cast<formseher::AlgorithmConfigDialog*>(&houghDialog1));
+//    w.registerAlgorithmConfigDialog(2, "Hough", static_cast<formseher::AlgorithmConfigDialog*>(&houghDialog2));
 
-    w.show();
+//    w.show();
 
-    return a.exec();
+//    return a.exec();
 }
