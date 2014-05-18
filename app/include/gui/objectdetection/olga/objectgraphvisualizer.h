7b66ca2df32efb68ab687ab49ac4d6c3e1b29c48
namelessvoid <pommesdiefritte@gmx.de>
2014-05-18 16:56:32 +0200

    Added ObjectGraphVisualizer class.

---
+++ app/include/gui/objectdetection/olga/objectgraphvisualizer.h
@@ -0,0 +1,20 @@
+#ifndef FS_OBJECTGRAPHVISUALIZER_H
+#define FS_OBJECTGRAPHVISUALIZER_H
+
+#include <opencv2/core/core.hpp>
+
+namespace formseher
+{
+
+class ObjectGraph;
+
+class ObjectGraphVisualizer
+{
+public:
+    ObjectGraphVisualizer();
+
+    static void drawGraphOnMatrix(const ObjectGraph* graph, cv::InputArray matrix);
+};
+
+} // namespace formseher
+#endif // FS_OBJECTGRAPHVISUALIZER_H
---
+++ app/src/gui/objectdetection/olga/objectgraphvisualizer.cpp
@@ -0,0 +1,15 @@
+#include "gui/objectdetection/olga/objectgraphvisualizer.h"
+
+namespace formseher
+{
+
+ObjectGraphVisualizer::ObjectGraphVisualizer()
+{
+}
+
+void ObjectGraphVisualizer::drawGraphOnMatrix(const ObjectGraph *graph, cv::InputArray matrix)
+{
+
+}
+
+} // namespace formseher
