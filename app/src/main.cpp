d5c283e9b48c6fcceb9f9f679ec9f5ecb6eb1201
namelessvoid <pommesdiefritte@gmx.de>
2014-05-19 16:44:17 +0200

    Restored original app main.cpp (sry about breaking it :/ ).

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -13,38 +13,23 @@
 
 int main(int argc, char *argv[])
 {
-    formseher::ObjectGraph objectGraph;
-    formseher::ObjectGraphNode* node1 = objectGraph.insertNode(10, 100);
-    formseher::ObjectGraphNode* node2 = objectGraph.insertNode(300, 50);
-    formseher::ObjectGraphNode* node3 = objectGraph.insertNode(30, 150);
-    formseher::ObjectGraphNode* node4 = objectGraph.insertNode(50, 100);
-    objectGraph.insertEdge(node1, node2);
-    objectGraph.insertEdge(node2, node3);
-    objectGraph.insertEdge(node1, node3);
-    objectGraph.insertEdge(node4, node3);
+    QApplication a(argc, argv);
 
-    cv::Mat graphMatrix;
-    formseher::ObjectGraphVisualizer::drawGraphOnMatrix(&objectGraph, graphMatrix);
-    cv::imshow("object graph", graphMatrix);
-    cv::waitKey(0);
+    formseher::EDLConfigDialog edlDialog1;
+    formseher::EDLConfigDialog edlDialog2;
 
-//    QApplication a(argc, argv);
+    formseher::HoughConfigDialog houghDialog1;
+    formseher::HoughConfigDialog houghDialog2;
 
-//    formseher::EDLConfigDialog edlDialog1;
-//    formseher::EDLConfigDialog edlDialog2;
-
-//    formseher::HoughConfigDialog houghDialog1;
-//    formseher::HoughConfigDialog houghDialog2;
-
-//    formseher::MainWindow w;
+    formseher::MainWindow w;
 
 
-//    w.registerAlgorithmConfigDialog(1, "EDL", static_cast<formseher::AlgorithmConfigDialog*>(&edlDialog1));
-//    w.registerAlgorithmConfigDialog(2, "EDL", static_cast<formseher::AlgorithmConfigDialog*>(&edlDialog2));
-//    w.registerAlgorithmConfigDialog(1, "Hough", static_cast<formseher::AlgorithmConfigDialog*>(&houghDialog1));
-//    w.registerAlgorithmConfigDialog(2, "Hough", static_cast<formseher::AlgorithmConfigDialog*>(&houghDialog2));
+    w.registerAlgorithmConfigDialog(1, "EDL", static_cast<formseher::AlgorithmConfigDialog*>(&edlDialog1));
+    w.registerAlgorithmConfigDialog(2, "EDL", static_cast<formseher::AlgorithmConfigDialog*>(&edlDialog2));
+    w.registerAlgorithmConfigDialog(1, "Hough", static_cast<formseher::AlgorithmConfigDialog*>(&houghDialog1));
+    w.registerAlgorithmConfigDialog(2, "Hough", static_cast<formseher::AlgorithmConfigDialog*>(&houghDialog2));
 
-//    w.show();
+    w.show();
 
-//    return a.exec();
+    return a.exec();
 }
