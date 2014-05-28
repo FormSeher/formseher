c46853454571c796a8af42c389559b75e2f19d49
namelessvoid <pommesdiefritte@gmx.de>
2014-05-28 14:53:23 +0200

    Added some todo-hints to Haff::calculate().

--- algorithms/src/objectdetection/haff/haff.cpp
+++ algorithms/src/objectdetection/haff/haff.cpp
@@ -15,15 +15,22 @@ std::vector<Object*> Haff::calculate(std::vector<Line> detectedLines)
 {
     std::multiset<Hypothesis> oldHypotheses;
     std::multiset<Hypothesis> newHypotheses;
+    std::multiset<Hypothesis> likelyHypotheses;
 
     for(auto model : database.getObjects())
     {
         for(auto modelLine : model->getLines())
         {
+            // TODO: Clearify how to construct a "non-matching" hypothesis value-key pair?
+
+            // Hypothesis h;
+            // h.addLineMatch(0, modelLine);
+            // h.calculateRating()
+            // newHypotheses.insert(h);
+
             for(auto detectedLine : detectedLines)
             {
                 // TODO: Ugly code duplication. How to improve?
-                // TODO: Clearify how to construct a "non-matching" hypothesis value-key pair.
 
                 // Construct newHypotheses in first iteration step.
                 if ( modelLine == model->getLines().front() )
@@ -46,6 +53,8 @@ std::vector<Object*> Haff::calculate(std::vector<Line> detectedLines)
                     {
                         if ( ! oldHypothesis.containsLine( &detectedLine ) )
                         {
+                            // TODO: Create a copy of oldHypothesis
+
                             // Create new Hypothesis
                             oldHypothesis.addLineMatch(&detectedLine, modelLine);
 
@@ -61,11 +70,15 @@ std::vector<Object*> Haff::calculate(std::vector<Line> detectedLines)
                 // Filter newHypotheses for best ones (do not forget to destory
                 // old hypohteses and new hypotheses which are no longer used!)
             }
-
         }
+
+        // TODO: Add likliest hypothesis to likelyHyptohesis set.
     }
 
     std::vector<Object*> detectedObjects;
+
+    // Create Objects
+
     return detectedObjects;
 }
 
