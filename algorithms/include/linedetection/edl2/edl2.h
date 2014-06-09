61cf9132a96e1e9dfa0f2b3b811afa2b821d7bc7
scarracs <mark.bonfert@hs-augsburg.de>
2014-06-09 03:36:04 +0200

    added additonal commentaries for doxy file.

--- algorithms/include/linedetection/edl2/edl2.h
+++ algorithms/include/linedetection/edl2/edl2.h
@@ -31,25 +31,93 @@ public:
     std::vector<Line> calculate(cv::InputArray _image);
 
 private:
+
+    /**
+    * @brief calculates the gradientMagnitudes for the the given image
+    */
+
     void calcGrad();
+
+    /**
+    * @brief finds the anchorpoints in the calculated edge image
+    * @param Vector to save the found anchorpoints to
+    */
 
     void findAnchors(std::vector<cv::Point> &anchors);
 
+    /**
+    * @brief connects the found anchors with another
+    * @param Vector with found anchors
+    * @param Vector to save found lines to
+    */
+
     void routeAnchors(std::vector<cv::Point>& anchorPoints, std::vector<Line> &result);
+
+    /**
+    * @brief walks from given point to the next point and saves the routed points as a lineSegment.
+    * @param Point to start walking from
+    * @param Vector with all found lineSegments
+    */
 
     void walkFromAnchor(cv::Point& anchorPoint, std::vector<std::list<cv::Point*>*>& lineSegments);
 
+    /**
+    * @brief tells if the orientation of a given vector is HORIZONTAL or VERTICAL
+    * @param Vector
+    * @param Vector with all found lineSegments
+    */
+
     bool getOrientation(cv::Vec2s &v1);
+
+    /**
+    * @brief finds the nextPoint to walk to, considering the orientation.
+    * @param Point to start the search for a possible nextPoint
+    * @param Point object for the chosen subdirection left/right/up/down
+    * @return Point the nextPoint. If no adeauqte nextPoint is found the given point is returned.
+    */
 
     cv::Point* getNextPoint(cv::Point& currentPoint, cv::Point& subDirection);
 
+    /**
+    * @brief checks if the point is still in the image
+    * @param Point to check
+    * @return bool value
+    */
+
     bool isOutOfBounds(cv::Point &point);
+
+    /**
+    * @brief checks if the point is still in the image
+    * @param Integer coordinate of the x value
+    * @param Integer coordinate of the y value
+    * @return bool value
+    */
 
     bool isOutOfBounds(int x, int y);
 
+    /**
+    * @brief helper method to get the the vector from the sobel image
+    * @param Point for which the vector is needed
+    * @return Vector of the given point
+    */
+
     cv::Vec2s* getSobelVector(cv::Point &point);
 
+    /**
+    * @brief helper method to get the the vector from the sobel image
+    * @param Integer value of row
+    * @param Integer value of column
+    * @return Vector of the given coordinate
+    */
+
     cv::Vec2s* getSobelVector(int row, int column);
+
+    /**
+    * @brief gets the angle beetween two vectors as radiant value
+    * @param Vector one
+    * @param Vector two
+    * @return Double value of the calculated radiant value
+    */
 
     double getAngleBetweenVectors(cv::Vec2s &v1, cv::Vec2s &v2);
 
