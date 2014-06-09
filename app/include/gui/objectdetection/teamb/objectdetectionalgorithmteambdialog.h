4db34a9a5bd9907a51daca93052d100809bc1012
Michael Hrstka <micovic@web.de>
2014-06-09 22:48:15 +0200

    created dialog for teamB object detection algorithm

--- app/include/gui/objectdetection/teamb/objectdetectionalgorithmteambdialog.h
+++ app/include/gui/objectdetection/teamb/objectdetectionalgorithmteambdialog.h
@@ -1,4 +1,19 @@
 #ifndef OBJECTDETECTIONALGORITHMTEAMBDIALOG_H
 #define OBJECTDETECTIONALGORITHMTEAMBDIALOG_H
 
+#include "gui/objectdetection/objectdetectionalgorithmconfigdialog.h"
+
+namespace formseher
+{
+
+class ObjectDetectionAlgorithmTeamBDialog : public ObjectDetectionAlgorithmConfigDialog
+{
+public:
+    ObjectDetectionAlgorithmTeamBDialog();
+
+    ObjectDetectionAlgorithm* createAlgorithm();
+};
+
+} // namespace formseher
+
 #endif // OBJECTDETECTIONALGORITHMTEAMBDIALOG_H
--- app/src/gui/objectdetection/teamb/objectdetectionalgorithmteambdialog.cpp
+++ app/src/gui/objectdetection/teamb/objectdetectionalgorithmteambdialog.cpp
@@ -1,4 +1,21 @@
 #ifndef OBJECTDETECTIONALGORITHMTEAMBDIALOG_CPP
 #define OBJECTDETECTIONALGORITHMTEAMBDIALOG_CPP
 
+#include "gui/objectdetection/teamb/objectdetectionalgorithmteambdialog.h"
+#include "objectdetection/teamb/objectdetectionalgorithmteamb.h"
+
+namespace formseher
+{
+
+ObjectDetectionAlgorithmTeamBDialog::ObjectDetectionAlgorithmTeamBDialog()
+{
+}
+
+ObjectDetectionAlgorithm *ObjectDetectionAlgorithmTeamBDialog::createAlgorithm()
+{
+    return new ObjectDetectionAlgorithmTeamB();
+}
+
+} // namespace formseher
+
 #endif // OBJECTDETECTIONALGORITHMTEAMBDIALOG_CPP
