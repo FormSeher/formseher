5cd0d2fce4fbe496695e9b560c533e7a91088d65
Michael Hrstka <micovic@web.de>
2014-06-15 21:48:27 +0200

    added gui config dialog teamb

--- app/include/gui/objectdetection/teamb/objectdetectionalgorithmteambdialog.h
+++ app/include/gui/objectdetection/teamb/objectdetectionalgorithmteambdialog.h
@@ -1,19 +1,34 @@
 #ifndef OBJECTDETECTIONALGORITHMTEAMBDIALOG_H
 #define OBJECTDETECTIONALGORITHMTEAMBDIALOG_H
 
+#include <QDialog>
 #include "gui/objectdetection/objectdetectionalgorithmconfigdialog.h"
+#include "objectdetection/objectdetectionalgorithm.h"
+
+namespace Ui {
+class ObjectDetectionAlgorithmTeamBDialog;
+}
 
 namespace formseher
 {
-
 class ObjectDetectionAlgorithmTeamBDialog : public ObjectDetectionAlgorithmConfigDialog
 {
+    Q_OBJECT
+
 public:
-    ObjectDetectionAlgorithmTeamBDialog();
-
+    explicit ObjectDetectionAlgorithmTeamBDialog(QWidget *parent = 0);
+    ~ObjectDetectionAlgorithmTeamBDialog();
     ObjectDetectionAlgorithm* createAlgorithm();
-};
 
-} // namespace formseher
+private slots:
+    // These slots just trigger the AlgorithmConfigDialog.configChanged() signal.
+    void on_minRating_valueChanged(int);
+    void on_maxAngleThreshold_valueChanged(double);
+    void on_maxDistanceThreshold_valueChanged(double);
+
+private:
+    Ui::ObjectDetectionAlgorithmTeamBDialog *ui;
+};
+}
 
 #endif // OBJECTDETECTIONALGORITHMTEAMBDIALOG_H
