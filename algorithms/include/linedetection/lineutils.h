579f3e742f6bbe245e13de2092b87e2deaa1f3bc
Antonio Grieco <antonio.grieco@hs-augsburg.de>
2014-06-18 17:03:38 +0200

    Added skeleton for implementing a line combination algorithm. @tmotu it's your time ;)

---
+++ algorithms/include/linedetection/lineutils.h
@@ -0,0 +1,23 @@
+#ifndef LINEUTILS_H
+#define LINEUTILS_H
+
+#include "line.h"
+
+namespace formseher {
+
+class LineUtils
+{
+public:
+    LineUtils();
+
+    /**
+     * @brief combineLines Combines lines in the given vector that represent logically the same line.
+     * @param lineList Lines from linedetection algorithm.
+     * @return Vector that contains just the combined lines.
+     */
+    static std::vector<Line> combineLines(std::vector<Line> lineList);
+};
+
+} // namesspace formseher
+
+#endif // LINEUTILS_H
---
+++ algorithms/src/linedetection/lineutils.cpp
@@ -0,0 +1,12 @@
+#include "linedetection/lineutils.h"
+#include "line.h"
+
+namespace formseher
+{
+
+std::vector<Line> LineUtils::combineLines(std::vector<Line> lineList)
+{
+
+}
+
+} // namespace formseher
