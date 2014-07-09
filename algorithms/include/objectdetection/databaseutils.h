de36c34d5179918196e2209a3b6b30623fde977c
Michael Hrstka <micovic@web.de>
2014-07-09 18:06:40 +0200

    added doxygen comments for database utils

--- algorithms/include/objectdetection/databaseutils.h
+++ algorithms/include/objectdetection/databaseutils.h
@@ -14,10 +14,33 @@ namespace formseher
 
     public:
 
+        /**
+         * @brief DatabaseUtils constructor
+         * @param path to existing or to be created database file
+         */
         DatabaseUtils(std::string path);
+
+        /**
+         * @brief reads in database file from path specified in constructor
+         * @return read in models as vector from database file
+         */
         std::vector<Model> read();
+
+        /**
+         * @brief write added Objects to database file to path specified in constructor
+         */
         void write();
+
+        /**
+         * @brief adds object to database - reminder: use write() after in the end in order to safe new altered database
+         * @param objectToAdd
+         */
         void addObject(Object objectToAdd);
+
+        /**
+         * @brief removes a object from database - reminder: use write() in the end in order to safe altered database
+         * @param objectToRemove
+         */
         void removeObject(Object objectToRemove);
 
     private:
